839970f8572b06ccf991e055fddd6a389a472a7e
Roger King <rogr.king@gmail.com>
2021-01-14 10:00:24 -0500

    feat: find image from dockerhub

---
+++ models/docker_hub.go
@@ -0,0 +1,45 @@
+package models
+
+import "fmt"
+
+// DockerHubCategory -
+type DockerHubCategory struct {
+	Name  string `json:"name"`
+	Label string `json:"label"`
+}
+
+// DockerHubPublisher -
+type DockerHubPublisher struct {
+	ID   string `json:"id"`
+	Name string `json:"Name"`
+}
+
+// DockerHubImage - represents a response for finding an image from dockerhub
+type DockerHubImage struct {
+	ID               string              `json:"id"`
+	Name             string              `json:"name"`
+	Slug             string              `json:"slug"`
+	Type             string              `json:"type"`
+	Source           string              `json:"source"`
+	Popularity       int                 `json:"popularity"`
+	Categories       []DockerHubCategory `json:"categories"`
+	Publisher        DockerHubPublisher  `json:"publisher"`
+	ShortDescription string              `json:"short_description"`
+	LongDescription  string              `json:"long_description"`
+	CreatedAt        string              `json:"created_at"`
+	UpdatedAt        string              `json:"updated_at"`
+}
+
+// IsDatabase -
+func (d *DockerHubImage) IsDatabase() bool {
+	isDb := false
+
+	for _, c := range d.Categories {
+		if c.Label == "database" {
+			isDb = true
+			break
+		}
+	}
+	fmt.Print("found db")
+	return isDb
+}
